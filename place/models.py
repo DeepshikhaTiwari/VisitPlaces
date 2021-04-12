6dce9f5ee99e83acaf291dbd4ec5be9b6955e88a
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-12 15:21:52 +0530

    Remove phone field

--- place/models.py
+++ place/models.py
@@ -4,10 +4,9 @@ from django.urls import reverse
 
 class Place(models.Model):
     title = models.CharField(max_length=150)
-    location = models.PointField(default='POINT(0, 0)', srid=4326)
-    description = models.TextField(null=True, blank=True, max_length=200)
+    location = models.PointField(srid=4326, blank=True, null=True)
+    description = models.TextField(null=True, blank=True, max_length=1000)
     address = models.CharField(max_length=225, blank=True, null=True)
-    phone = models.IntegerField()
     city = models.CharField(max_length=15)
     type = models.CharField(max_length=100)
 
